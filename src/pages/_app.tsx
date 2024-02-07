d7cc2c8faf6c8ab4c66f968b80d4e1f5c7ca39ef
tmatsiievskyi <taras.matsiievskyi>
2024-02-07 22:52:58 +0200

    vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/modules/Layout";
 import { appWithTranslation } from "next-i18next";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout {...pageProps}>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 };

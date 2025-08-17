49bd56f90e87a3310c616c441a1e92e64d58995d
tmatsiievskyi <taras.matsiievskyi>
2025-08-18 00:36:11 +0300

    update

--- src/modules/SubService/containers/SubServiceContainer.tsx
+++ src/modules/SubService/containers/SubServiceContainer.tsx
@@ -225,6 +225,10 @@ export const SubServiceContainer = () => {
                                                     {t(subservice.priceSuffix)}
                                                   </span>
                                                 )}
+                                                <span className="ml-10">
+                                                  {" "}
+                                                  {subservice._id}
+                                                </span>
                                               </div>
                                             );
                                           }
